abbc95204b6dde341900142879462870d7b11944
Gerald Ramos <ramosgerald255@gmail.com>
2022-01-07 08:03:23 +0800

    deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -17,8 +17,8 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:ramosgerald255/<USERNAME>.github.io.git main
+git push -f git@github.com:ramosgerald255/ramosgerald255.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:ramosgerald255/ramosgerald255.github.io.git main:gh-pages
+# git push -f git@github.com:ramosgerald255/ramosgerald255.github.io.git main:gh-pages
 cd -
